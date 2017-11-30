0e75ba5216ed843c7fa63ab33de95f462ddc5ed6
Cory <channon1@umd.edu>
2017-11-29 20:44:09 -0500

    create account page linked and wokring

--- app/src/main/java/com/example/cory/feedthekitty/CreateAccount.java
+++ app/src/main/java/com/example/cory/feedthekitty/CreateAccount.java
@@ -1,20 +1,35 @@
 package com.example.cory.feedthekitty;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.Task;
 
 public class CreateAccount extends AppCompatActivity {
 
     EditText mDisplayName, mEmailAddress, mPassword;
     Button mSubmitButton;
+    private FirebaseAuth mAuth;
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
         return password.length() > 4;
+    }
+    private void focusEmail(){
+        View focusView = null;
+        focusView = mEmailAddress;
+        focusView.requestFocus();
     }
 
     private boolean isEmailValid(String email) {
@@ -23,9 +38,10 @@ public class CreateAccount extends AppCompatActivity {
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
-
+        mAuth = FirebaseAuth.getInstance();
         mDisplayName = (EditText) findViewById(R.id.display_name_text_view);
         mEmailAddress = (EditText) findViewById(R.id.enter_email_field);
         mPassword = (EditText) findViewById(R.id.enter_password_field);
@@ -35,8 +51,70 @@ public class CreateAccount extends AppCompatActivity {
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Reset errors.
+//                if (mAuthTask != null) {
+//                    return;
+//                }
+
+                // Reset errors.
+                mEmailAddress.setError(null);
+                mPassword.setError(null);
+
+                // Store values at the time of the login attempt.
+                String email = mEmailAddress.getText().toString();
+                String password = mPassword.getText().toString();
+
+                boolean cancel = false;
+                View focusView = null;
+
+                // Check for a valid password, if the user entered one.
+                if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+                    mPassword.setError(getString(R.string.error_invalid_password));
+                    focusView = mPassword;
+                    cancel = true;
+                }
+
+                // Check for a valid email address.
+                if (TextUtils.isEmpty(email)) {
+                    mEmailAddress.setError(getString(R.string.error_field_required));
+                    focusView = mEmailAddress;
+                    cancel = true;
+                } else if (!isEmailValid(email)) {
+                    mEmailAddress.setError(getString(R.string.error_invalid_email));
+                    focusView = mEmailAddress;
+                    cancel = true;
+                }
+
+                if (cancel) {
+                    // There was an error; don't attempt login and focus the first
+                    // form field with an error.
+                    focusView.requestFocus();
+                } else {
+                    // Show a progress spinner, and kick off a background task to
+                    // perform the user login attempt.
+                    //showProgress(true);
+                    //            mAuthTask = new UserLoginTask(email, password);
+                    //            mAuthTask.execute((Void) null);
+                    //Log.i(TAG, "trying to make contact");
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CreateAccount.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            //Log.i(TAG, "we here bois too");
+                            if (task.isSuccessful()) {
+                                //Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(CreateAccount.this, "Thanks for signing up, " + user.getEmail(), Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Toast.makeText(CreateAccount.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                                focusEmail();
+                            }
+                        }
+                    });
+                }
+                //end onClick
+                finish();
             }
+            //end setOnCLickListener
         });
     }
 }
--- app/src/main/java/com/example/cory/feedthekitty/LoginActivity.java
+++ app/src/main/java/com/example/cory/feedthekitty/LoginActivity.java
@@ -354,68 +354,68 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private void attemptSignup() {
-//        Intent intent = new Intent(LoginActivity.this, CreateAccount.class);
-//        startActivity(intent);
-        if (mAuthTask != null) {
-            return;
-        }
-
-        // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            //showProgress(true);
-//            mAuthTask = new UserLoginTask(email, password);
-//            mAuthTask.execute((Void) null);
-            Log.i(TAG, "trying to make contact");
-            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    Log.i(TAG, "we here bois too");
-                    if(task.isSuccessful()){
-                        Log.d(TAG, "createUserWithEmail:success");
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        Toast.makeText(LoginActivity.this, "Thanks for signing up, " + user.getEmail(), Toast.LENGTH_SHORT).show();
-
-                    }else{
-                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
-                        focusEmail();
-                    }
-                }
-            });
-        }
+        Intent intent = new Intent(LoginActivity.this, CreateAccount.class);
+        startActivity(intent);
+//        if (mAuthTask != null) {
+//            return;
+//        }
+//
+//        // Reset errors.
+//        mEmailView.setError(null);
+//        mPasswordView.setError(null);
+//
+//        // Store values at the time of the login attempt.
+//        String email = mEmailView.getText().toString();
+//        String password = mPasswordView.getText().toString();
+//
+//        boolean cancel = false;
+//        View focusView = null;
+//
+//        // Check for a valid password, if the user entered one.
+//        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+//            mPasswordView.setError(getString(R.string.error_invalid_password));
+//            focusView = mPasswordView;
+//            cancel = true;
+//        }
+//
+//        // Check for a valid email address.
+//        if (TextUtils.isEmpty(email)) {
+//            mEmailView.setError(getString(R.string.error_field_required));
+//            focusView = mEmailView;
+//            cancel = true;
+//        } else if (!isEmailValid(email)) {
+//            mEmailView.setError(getString(R.string.error_invalid_email));
+//            focusView = mEmailView;
+//            cancel = true;
+//        }
+//
+//        if (cancel) {
+//            // There was an error; don't attempt login and focus the first
+//            // form field with an error.
+//            focusView.requestFocus();
+//        } else {
+//            // Show a progress spinner, and kick off a background task to
+//            // perform the user login attempt.
+//            //showProgress(true);
+////            mAuthTask = new UserLoginTask(email, password);
+////            mAuthTask.execute((Void) null);
+//            Log.i(TAG, "trying to make contact");
+//            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+//                @Override
+//                public void onComplete(@NonNull Task<AuthResult> task) {
+//                    Log.i(TAG, "we here bois too");
+//                    if(task.isSuccessful()){
+//                        Log.d(TAG, "createUserWithEmail:success");
+//                        FirebaseUser user = mAuth.getCurrentUser();
+//                        Toast.makeText(LoginActivity.this, "Thanks for signing up, " + user.getEmail(), Toast.LENGTH_SHORT).show();
+//
+//                    }else{
+//                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+//                        focusEmail();
+//                    }
+//                }
+//            });
+//        }
     }
 
     private void focusEmail(){
