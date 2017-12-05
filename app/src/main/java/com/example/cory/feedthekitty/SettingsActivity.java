e800e45dd6f09a0a14d932155c42e8fd6fcd5b05
aksdad <akshay@aksdad.com>
2017-12-05 01:36:41 -0500

    cleaned up settings made ui colors better

--- app/src/main/java/com/example/cory/feedthekitty/SettingsActivity.java
+++ app/src/main/java/com/example/cory/feedthekitty/SettingsActivity.java
@@ -117,15 +117,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
      * @see #sBindPreferenceSummaryToValueListener
      */
     private static void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
 
-        // Trigger the listener immediately with the preference's
-        // current value.
-        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
     }
 
     @Override
